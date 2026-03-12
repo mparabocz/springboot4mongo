49efd788a98464f9d6e9436fe0bc0890d6600208
Marcelo Parabocz <macerno@gmail.com>
2026-03-12 11:32:40 -0300

    user find by id

--- src/main/java/com/mparabocz/workshopmongo/services/UserService.java
+++ src/main/java/com/mparabocz/workshopmongo/services/UserService.java
@@ -1,21 +1,28 @@
 package com.mparabocz.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mparabocz.workshopmongo.domain.User;
 import com.mparabocz.workshopmongo.repository.UserRepository;
+import com.mparabocz.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
 	}
 
 }
