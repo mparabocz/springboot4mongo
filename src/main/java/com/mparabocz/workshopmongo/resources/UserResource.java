170bb24213089b5ea32ceba98b2e344e7fd0a85f
Marcelo Parabocz <macerno@gmail.com>
2026-03-12 11:17:08 -0300

    feat: use of dto for users

--- src/main/java/com/mparabocz/workshopmongo/resources/UserResource.java
+++ src/main/java/com/mparabocz/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.mparabocz.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mparabocz.workshopmongo.domain.User;
+import com.mparabocz.workshopmongo.dto.UserDTO;
 import com.mparabocz.workshopmongo.services.UserService;
 
 @RestController
@@ -17,10 +19,11 @@ public class UserResource {
 
 	@Autowired
 	private UserService service;
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
