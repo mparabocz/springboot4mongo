8ebf8a78110070770232fbebf1eee96baed4051c
Marcelo Parabocz <macerno@gmail.com>
2026-03-13 17:59:58 -0300

    feat: entity Post implementation

--- src/main/java/com/mparabocz/workshopmongo/config/Instantiation.java
+++ src/main/java/com/mparabocz/workshopmongo/config/Instantiation.java
@@ -1,30 +1,47 @@
 package com.mparabocz.workshopmongo.config;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.mparabocz.workshopmongo.domain.Post;
 import com.mparabocz.workshopmongo.domain.User;
+import com.mparabocz.workshopmongo.repository.PostRepository;
 import com.mparabocz.workshopmongo.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
-	
+
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private PostRepository postRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		userRepository.deleteAll();
-		
+		postRepository.deleteAll();
+
 		User abraao = new User(null, "Abraao", "abraao@gmail.com");
 		User braulio = new User(null, "Braulio", "brauliao@gmail.com");
 		User claudia = new User(null, "Claudia", "claudinha@gmail.com");
 		userRepository.saveAll(Arrays.asList(abraao, braulio, claudia));
-		
+
+		Post p1 = new Post(null, LocalDate.of(2026, 1, 1), "Exciting!",
+				"I'm going out to investigate some rumors around my city. Wish me luck.", abraao);
+		Post p2 = new Post(null, LocalDate.of(2025, 11, 30), "Beware.",
+				"Beware of the old man in a profession where people die young.", abraao);
+		Post p3 = new Post(null, LocalDate.of(2024, 9, 9), "Missing you", "You said you'd grow old with me.", braulio);
+		Post p4 = new Post(null, LocalDate.of(2026, 2, 10), "Right Light",
+				"Light leaves right time; right light leaves time right, while time leaves light right where right time leaves light.",
+				claudia);
+		postRepository.saveAll(Arrays.asList(p1, p2, p3, p4));
+
 	}
 
 }
