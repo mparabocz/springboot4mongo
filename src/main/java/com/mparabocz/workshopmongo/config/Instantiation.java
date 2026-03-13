853854270cba10addefc027896fdc60da19355dd
Marcelo Parabocz <macerno@gmail.com>
2026-03-13 18:06:14 -0300

    feat: author dto implementation

--- src/main/java/com/mparabocz/workshopmongo/config/Instantiation.java
+++ src/main/java/com/mparabocz/workshopmongo/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.mparabocz.workshopmongo.domain.Post;
 import com.mparabocz.workshopmongo.domain.User;
+import com.mparabocz.workshopmongo.dto.AuthorDTO;
 import com.mparabocz.workshopmongo.repository.PostRepository;
 import com.mparabocz.workshopmongo.repository.UserRepository;
 
@@ -33,13 +34,14 @@ public class Instantiation implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(abraao, braulio, claudia));
 
 		Post p1 = new Post(null, LocalDate.of(2026, 1, 1), "Exciting!",
-				"I'm going out to investigate some rumors around my city. Wish me luck.", abraao);
+				"I'm going out to investigate some rumors around my city. Wish me luck.", new AuthorDTO(abraao));
 		Post p2 = new Post(null, LocalDate.of(2025, 11, 30), "Beware.",
-				"Beware of the old man in a profession where people die young.", abraao);
-		Post p3 = new Post(null, LocalDate.of(2024, 9, 9), "Missing you", "You said you'd grow old with me.", braulio);
+				"Beware of the old man in a profession where people die young.", new AuthorDTO(abraao));
+		Post p3 = new Post(null, LocalDate.of(2024, 9, 9), "Missing you", "You said you'd grow old with me.",
+				new AuthorDTO(braulio));
 		Post p4 = new Post(null, LocalDate.of(2026, 2, 10), "Right Light",
 				"Light leaves right time; right light leaves time right, while time leaves light right where right time leaves light.",
-				claudia);
+				new AuthorDTO(claudia));
 		postRepository.saveAll(Arrays.asList(p1, p2, p3, p4));
 
 	}
