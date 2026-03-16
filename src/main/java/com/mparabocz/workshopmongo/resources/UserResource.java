4ed882575bce23845fafd849176b8e4a2b5b33be
Marcelo Parabocz <macerno@gmail.com>
2026-03-16 09:44:02 -0300

    feat: endpoint for posts per user

--- src/main/java/com/mparabocz/workshopmongo/resources/UserResource.java
+++ src/main/java/com/mparabocz/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.mparabocz.workshopmongo.domain.Post;
 import com.mparabocz.workshopmongo.domain.User;
 import com.mparabocz.workshopmongo.dto.UserDTO;
 import com.mparabocz.workshopmongo.services.UserService;
@@ -61,5 +62,12 @@ public class UserResource {
 		obj = service.update(obj);
 
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
