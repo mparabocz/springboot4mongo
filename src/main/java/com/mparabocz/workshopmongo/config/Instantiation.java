f7726cfc9f89a7763adc3f4011f69fb5e56614d6
Marcelo Parabocz <macerno@gmail.com>
2026-03-16 10:03:31 -0300

    feat: add comments to posts

--- src/main/java/com/mparabocz/workshopmongo/config/Instantiation.java
+++ src/main/java/com/mparabocz/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.mparabocz.workshopmongo.domain.Post;
 import com.mparabocz.workshopmongo.domain.User;
 import com.mparabocz.workshopmongo.dto.AuthorDTO;
+import com.mparabocz.workshopmongo.dto.CommentDTO;
 import com.mparabocz.workshopmongo.repository.PostRepository;
 import com.mparabocz.workshopmongo.repository.UserRepository;
 
@@ -42,6 +43,21 @@ public class Instantiation implements CommandLineRunner {
 		Post p4 = new Post(null, LocalDate.of(2026, 2, 10), "Right Light",
 				"Light leaves right time; right light leaves time right, while time leaves light right where right time leaves light.",
 				new AuthorDTO(claudia));
+
+		CommentDTO c1 = new CommentDTO("Watch out!", LocalDate.of(2026, 1, 2), new AuthorDTO(braulio));
+		CommentDTO c2 = new CommentDTO("Thats sick! GL", LocalDate.of(2026, 1, 4), new AuthorDTO(claudia));
+		CommentDTO c3 = new CommentDTO("...Interesting thought.", LocalDate.of(2025, 12, 1), new AuthorDTO(braulio));
+		CommentDTO c4 = new CommentDTO("Miss you too buddy haha", LocalDate.of(2024, 9, 11), new AuthorDTO(abraao));
+		CommentDTO c5 = new CommentDTO("I'm unsure if that's right or not tho.", LocalDate.of(2026, 2, 10),
+				new AuthorDTO(abraao));
+		CommentDTO c6 = new CommentDTO("I leave for one day and you hit us with this light? thats not right.",
+				LocalDate.of(2026, 2, 11), new AuthorDTO(abraao));
+
+		p1.getComments().addAll(Arrays.asList(c1, c2));
+		p2.getComments().addAll(Arrays.asList(c3));
+		p3.getComments().addAll(Arrays.asList(c4));
+		p4.getComments().addAll(Arrays.asList(c5, c6));
+
 		postRepository.saveAll(Arrays.asList(p1, p2, p3, p4));
 
 		abraao.getPosts().add(p1);
@@ -49,6 +65,7 @@ public class Instantiation implements CommandLineRunner {
 		braulio.getPosts().add(p3);
 		claudia.getPosts().add(p4);
 		userRepository.saveAll(Arrays.asList(abraao, braulio, claudia));
+
 	}
 
 }
