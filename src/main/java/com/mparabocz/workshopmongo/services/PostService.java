71d5ede85450fa56a6791c58fa18c5c69fd7661e
Marcelo Parabocz <macerno@gmail.com>
2026-03-16 09:49:57 -0300

    feat: endpoint for posts by id

---
+++ src/main/java/com/mparabocz/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.mparabocz.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mparabocz.workshopmongo.domain.Post;
+import com.mparabocz.workshopmongo.repository.PostRepository;
+import com.mparabocz.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
+	}
+
+}
